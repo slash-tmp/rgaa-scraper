30e322ec417a3c582d68250771644b916722a04c
Adrien BOUTIGNY <adrien.boutigny@slash-tmp.dev>
2021-05-04 11:52:33 +0200

    optimize imports for tree-shaking

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,6 @@
-import Crawler from 'crawler'
-import type { CrawlerRequestResponse } from 'crawler'
-
-import RgaaResultWrapper from './RgaaResultWrapper'
-import { parseCriteriaArticle, parseTestLi, parseTopicA } from './parser'
-import { RgaaCrawlerResult, RgaaRawCrawlerResult, RgaaRawTopic } from './types'
-
 export { default as RgaaResultWrapper } from './RgaaResultWrapper'
 export { default as RgaaCriterionWrapper } from './RgaaCriterionWrapper'
 export { default as RgaaTopicWrapper } from './RgaaTopicWrapper'
 export { default as RgaaTestWrapper } from './RgaaTestWrapper'
+export { default as crawlRgaa } from './crawl'
 export * from './types'
-
-const RGAA_URL =
-  'https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode-rgaa/criteres/'
-
-function queueAsPromise(
-  uri: string,
-  crawler: Crawler
-): Promise<CrawlerRequestResponse> {
-  return new Promise((resolve, reject) => {
-    crawler.direct({
-      uri,
-      callback(error, response) {
-        if (error) {
-          reject(error)
-        }
-        resolve(response)
-      },
-    })
-  })
-}
-
-function parseRgaaPage({ $ }: CrawlerRequestResponse): RgaaRawCrawlerResult {
-  const criteria = $('#criteres article')
-    .toArray()
-    .map(el => parseCriteriaArticle($(el)))
-
-  const tests = $('#criteres li[id*="test"]')
-    .toArray()
-    .map(el => parseTestLi($(el)))
-
-  const topics: RgaaRawTopic[] = $('ol#topics-list li a')
-    .toArray()
-    .map(el => parseTopicA($(el)))
-
-  return {
-    criteria,
-    tests,
-    topics,
-  }
-}
-
-/**
- * Scraps the RGAA website and returns a promise to an object representing
- * topics, criteria and tests.
- */
-export async function crawlRgaa(): Promise<RgaaCrawlerResult> {
-  const crawler = new Crawler({})
-
-  // fetch RGAA page
-  const res = await queueAsPromise(RGAA_URL, crawler)
-
-  // parse the page for criteria and tests
-  const data = parseRgaaPage(res)
-
-  return new RgaaResultWrapper(data)
-}
--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-import $ from 'cheerio'
+import * as $ from 'cheerio'
 import { RgaaRawCriterion, RgaaRawTest, RgaaRawTopic } from './types'
 import { reduceWhitespaces } from './utils'
 
