a21e2a73390e00cd528588c2f61c53ae9c323aca
Quentin Bellanger <hello@quentin-bellanger.com>
2021-05-10 18:25:48 +0200

    Create vue example project front-end

---
+++ examples/rgaa-tests-search/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Recherche des tests du RGAA</h1>
+    <form @submit.prevent="search">
+      <label for="search">Recherche textuelle</label>
+      <input v-model="searchValue" id="search" type="text" />
+      <button>Rechercher</button>
+      <button @click="reset" type="reset">Réinitialiser</button>
+    </form>
+
+    <ul>
+      <li v-for="test in filteredTests" :key="test.id">
+        <strong>{{ test.id }}</strong>
+        {{ test.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// TODO: code split
+import rgaaData from '../rgaa-data.json'
+import RgaaResultWrapper from '@slash-tmp/rgaa-scraper/build/esm/RgaaResultWrapper'
+
+const rgaa = new RgaaResultWrapper(rgaaData)
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      searchValue: '',
+      submittedSearch: '',
+    }
+  },
+  computed: {
+    filteredTests() {
+      return rgaa.tests({ search: this.submittedSearch })
+    },
+  },
+  methods: {
+    search() {
+      this.submittedSearch = this.searchValue
+    },
+    reset() {
+      this.submittedSearch = ''
+    },
+  },
+}
+</script>
